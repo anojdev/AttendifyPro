0dd93408c55fb7c6ef6fd8a142c8a62e0c6cd593
hariclerry <clerryha@gmail.com>
2024-06-18 17:05:25 -0500

    add comment

--- src/main/java/edu/miu/attendifypro/repository/StudentAttendanceRecordRepository.java
+++ src/main/java/edu/miu/attendifypro/repository/StudentAttendanceRecordRepository.java
@@ -8,6 +8,10 @@ import java.util.List;
 
 public interface StudentAttendanceRecordRepository extends JpaRepository<StudentAttendanceRecord, Long> {
 //    List<StudentAttendanceRecord> findByStudentId(long student_id);
-    @Query("select e from  StudentAttendanceRecord e inner join e.student s where s.firstName = :name")
+    @Query("select sa from  StudentAttendanceRecord sa inner join sa.student s where s.firstName = :name")
     List<StudentAttendanceRecord> findByStudentId(String name);
+
+    //uncomment once security is working
+//    @Query("select sa from  StudentAttendanceRecord sa inner join sa.student s where s.studentId = :studentId")
+//    List<StudentAttendanceRecord> findByStudentId(String studentId);
 }
